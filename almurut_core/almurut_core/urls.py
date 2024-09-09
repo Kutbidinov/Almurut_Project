369fdde1cb3266afbbc66cbd85b7ebe9806ff2d2
madaz1movsa <166149668+Kutbidinov@users.noreply.github.com>
2024-09-09 18:23:45 +0600

    !

--- almurut_core/almurut_core/urls.py
+++ almurut_core/almurut_core/urls.py
@@ -2,7 +2,7 @@
 URL configuration for almurut_core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,19 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from market.views import HomeView, Custom404View, FaqView, FavoritesView, LoginView, ProductDetailView, ProductListView, \
+    RegisterView, ShoppingCartView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('404_error/', Custom404View.as_view(), name = '404_error'),
+    path('faq_page/', FaqView.as_view(), name = 'faq_page'),
+    path('favorites/', FavoritesView.as_view(), name = 'favorites_list'),
+    path('home/', HomeView.as_view(), name = 'home'),
+    path('login_page/', LoginView.as_view(), name = 'login_page'),
+    path('product_detail/', ProductDetailView.as_view(), name = 'product_detail_list'),
+    path('product_list/', ProductListView.as_view(), name = 'product_list'),
+    path('register_page/', RegisterView.as_view(), name = 'register_page'),
+    path('shopping_cart/', ShoppingCartView.as_view(), name = 'shopping_cart_list' )
 ]
-
-
-
-# Добавляем обработку медиафайлов в общий список URL-паттернов
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
