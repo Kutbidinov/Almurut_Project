39e6e54712370991af3f62c548a5788e952623ea
madaz1movsa <166149668+Kutbidinov@users.noreply.github.com>
2024-09-18 18:43:48 +0600

    Changes

--- almurut_core/almurut_core/urls.py
+++ almurut_core/almurut_core/urls.py
@@ -19,28 +19,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from market.views import HomeView, FaqView, Error404View, FavoritesView, \
-     ProductDetailView, ProductListView, ShoppingCartView
 
 
-from users.views import UserRegisterView, UserMakeRegisterView, \
-    UserLoginView, UserMakeLoginView
-
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home/', HomeView.as_view(), name='Home-url'),
-    path('faq/', FaqView.as_view(), name='Faq-url'),
-    path('204_error/', Error404View.as_view()),
-    path('favorites/', FavoritesView.as_view()),
-    path('login/', UserLoginView.as_view()),
-    path('productDetail/', ProductDetailView.as_view()),
-    path('productList/', ProductListView.as_view(), name='Shop-url'),
-    path('shoppingCart/', ShoppingCartView.as_view()),
-    path('registration/', UserRegisterView.as_view(), name='registration-url'),
-    path('make-registration/', UserMakeRegisterView.as_view(), name='make-registration-url'),
-    path('make-login/', UserMakeLoginView.as_view(), name='make-login-url')
-
-]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- almurut_core/market/admin.py
+++ almurut_core/market/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
 
-from market.models import Product,ProductCategory,ProductGallery
+from market.models import Product, ProductCategory, ProductGallery
 
 
 
